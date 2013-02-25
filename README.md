6f70e95fc3a21ebefb5210e91b25a66dfcb0d380
JD Harrington <jd@jdharrington.net>
2013-02-24 23:25:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vagrant Puppet Module for Boxen
+
+## Usage
+
+```puppet
+include vagrant
+```
+
+## Required Puppet Modules
+
+* boxen
+* stdlib
---
+++ manifests/init.pp
@@ -0,0 +1,6 @@
+class vagrant {
+  package { 'Vagrant':
+    source   => "http://files.vagrantup.com/packages/476b19a9e5f499b5d0b9d4aba5c0b16ebe434311/Vagrant.dmg",
+    provider => 'pkgdmg'
+  }
+}
